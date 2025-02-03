2b35fcfaa9dd88b86ccfde4f87cfdc4bbce6a8f7
Osi ic <osiic.offcl@gmail.com>
2025-02-03 18:29:54 +0700

    feat: creating api DELETE series /api/series/[slug], and add a command in getSeries and deleteSeries

--- src/app/api/series/[slug]/route.js
+++ src/app/api/series/[slug]/route.js
@@ -2,21 +2,43 @@ import { prisma } from "@/prisma";
 import { authenticate } from "@/auth";
 import { NextResponse } from "next/server";
 
-export async function DELETE(request) {
+export async function DELETE(request, { params }) {
   try {
+    // Check Authorized
     if (!authenticate(request)) {
       return NextResponse.json({ error: "Unauthorized." }, { status: 401 });
     }
 
-    const dataSeries = await prisma.series.findMany({
-      where: { deleted: false },
-      orderBy: { updatedOn: "desc" }, // Mengurutkan berdasarkan waktu update terbaru
+    // Get slug
+    const { slug } = params;
+    // console.log(slug);
+
+    // Check if the data exists before trying to update
+    const existingData = await prisma.series.findUnique({
+      where: { slug },
     });
 
-    // console.log(dataSeries);
+    if (!existingData || existingData.deleted) {
+      return NextResponse.json({ error: "Data not found." }, { status: 404 });
+    }
 
-    return NextResponse.json({ data: dataSeries, helo: "jhkasd" });
-  } catch {
+    // Perform to delete series
+    const dataDeleted = await prisma.series.update({
+      where: { slug },
+      data: { deleted: true },
+    });
+    // console.log(dataDeleted)
+
+    return NextResponse.json({
+      data: {
+        title: dataDeleted?.title,
+        slug: dataDeleted?.slug,
+        deletedOn: dataDeleted?.updatedOn,
+      },
+      status: "Data has been deleted successfully.",
+    });
+  } catch (error) {
+    console.error(error);
     return NextResponse.json({ error: "Server error." }, { status: 500 });
   }
 }
--- src/app/api/series/route.js
+++ src/app/api/series/route.js
@@ -4,18 +4,19 @@ import { NextResponse } from "next/server";
 
 export async function GET(request) {
   try {
+    // Check Authorized
     if (!authenticate(request)) {
       return NextResponse.json({ error: "Unauthorized." }, { status: 401 });
     }
 
+    // Get all series
     const dataSeries = await prisma.series.findMany({
       where: { deleted: false },
       orderBy: { updatedOn: "desc" }, // Mengurutkan berdasarkan waktu update terbaru
     });
-
     // console.log(dataSeries);
 
-    return NextResponse.json({ data: dataSeries, helo: "jhkasd" });
+    return NextResponse.json({ data: dataSeries, status: "Data series successfully retrieved"});
   } catch {
     return NextResponse.json({ error: "Server error." }, { status: 500 });
   }
