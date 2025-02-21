57761952da44968e85fd2c1205567c5517bd8aa1
Osi ic <osiic.offcl@gmail.com>
2025-02-21 10:30:25 +0700

    feat: setup page sign-in and sign-out

--- src/app/(routes)/(protect)/sign-in/page.jsx
+++ src/app/(routes)/(protect)/sign-in/page.jsx
@@ -2,13 +2,19 @@ import { signIn } from "@/auth";
 
 export default function SignIn() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signIn("google", { redirectTo: "/dashboard" });
-      }}
-    >
-      <button type="submit">Sign In</button>
-    </form>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold mb-6">Welcome to Sign In Page</h1>
+
+      <form
+        action={async () => {
+          "use server";
+          await signIn("google", { redirectTo: "/dashboard" });
+        }}
+      >
+        <button className="px-6 py-3 bg-blue-600 rounded-lg hover:bg-blue-700 transition">
+          Sign In
+        </button>
+      </form>
+    </div>
   );
 }
--- src/app/(routes)/(protect)/sign-out/page.jsx
+++ src/app/(routes)/(protect)/sign-out/page.jsx
@@ -2,13 +2,19 @@ import { signOut } from "@/auth";
 
 export default function SignOut() {
   return (
-    <form
-      action={async () => {
-        "use server";
-        await signOut({ redirectTo: "/dashboard" });
-      }}
-    >
-      <button type="submit">Sign Out</button>
-    </form>
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
+      <h1 className="text-3xl font-bold mb-6">Are you sure you want to Sign Out?</h1>
+
+      <form
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: "/" });
+        }}
+      >
+        <button className="px-6 py-3 bg-red-600 rounded-lg hover:bg-red-700 transition">
+          Sign Out
+        </button>
+      </form>
+    </div>
   );
 }
