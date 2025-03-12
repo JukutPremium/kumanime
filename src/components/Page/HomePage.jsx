6ed00fbf136693213638e6dd1a689c3fcca0f164
Osi ic <osiic.offcl@gmail.com>
2025-03-12 21:42:45 +0700

    fix: just push

--- src/components/Page/HomePage.jsx
+++ src/components/Page/HomePage.jsx
@@ -1,41 +1,20 @@
-import Navbar from "@/components/Layout/Navbar";
-import Footer from "@/components/Layout/Footer";
 import HeroSlider from "@/components/Layout/Slider";
+
 import Link from "next/link";
 import Image from "next/image";
 
-const HomePage = () => {
+const HomePage = ({ updatedSeries, completedSeries }) => {
+  const formattedSliderData = updatedSeries.map((item) => ({
+    backgroundImage: item.banner,
+    title: item.title,
+    description: item.synopsis,
+    slug: `${process.env.NEXT_PUBLIC_BASE_URL}/series/${item.slug}`,
+    primaryButtonText: "Watch Now",
+  }));
+
   return (
     <>
-      {/* <section className="relative h-[calc(100vh-80px)] w-full">
-        <div className="absolute inset-0 bg-[url(../../public/banner.jpg)] bg-no-repeat bg-cover brightness-50" />
-        <div className="relative z-10 h-full w-full md:w-[900px] flex items-end justify-center">
-          <div className='p-4 sm:p-12 md:p-24 w-full'>
-            <div className='border-l-4 border-[#2FFF51] py-2'>
-              <h1 className="text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold pl-3 sm:pl-5">DAN DAN DAN DAN</h1>
-              <p className="text-white pl-4 sm:pl-6 text-sm sm:text-base lg:text-lg">Momo&apos;s childhood friend and first love, Jiji (Enjoji Jin), comes to Seiko for help because he is disturbed by strange events in his new house. Okarun, who just found out that Jiji is Momo&apos;s first love, can&apos;t hide her anxiety and tries to get away from Momo. At that moment, a human body model appears and runs with all her might in front of Okarun.</p>
-            </div>
-            <div className='pt-4 sm:pt-8 flex flex-col sm:flex-row gap-4'>
-              <button className="bg-[#2FFF51] px-6 sm:px-10 py-2 sm:py-3 rounded-full flex items-center justify-center space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-6 sm:w-6 text-black" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-                <span className='font-bold text-sm sm:text-base'>Watch Now</span>
-              </button>
-              <button className="border-4 border-[#2FFF51] px-6 sm:px-10 py-2 sm:py-3 rounded-full flex items-center justify-center space-x-2">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 sm:h-6 sm:w-6 text-[#2FFF51]" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M14.752 11.168l-3.197-2.132A1 1 0 0010 9.87v4.263a1 1 0 001.555.832l3.197-2.132a1 1 0 000-1.664z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-                <span className='text-[#2FFF51] font-bold text-sm sm:text-base'>Add To Watch List</span>
-              </button>
-            </div>
-          </div>
-        </div>
-      </section> */}
-
-      <HeroSlider />
+      <HeroSlider sliderData={formattedSliderData} />
 
       {/* New Update */}
       <section className="h-full w-full">
