f88a5495c31ef0677eb7232db84bc010240fa291
Osi ic <osiic.offcl@gmail.com>
2025-03-14 15:45:56 +0700

    fix: signin and signout

--- src/app/(routes)/(protect)/sign-in/page.jsx
+++ src/app/(routes)/(protect)/sign-in/page.jsx
@@ -6,16 +6,14 @@ export const metadata = {
 
 export default function SignIn() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white">
-      <h1 className="text-3xl font-bold mb-6">Welcome to Sign In Page</h1>
-
+    <div className="min-h-screen flex flex-col items-center justify-center text-white">
       <form
         action={async () => {
           "use server";
           await signIn("google", { redirectTo: "/dashboard" });
         }}
       >
-        <button className="px-6 py-3 bg-blue-600 rounded-lg hover:bg-blue-700 transition">
+        <button className="px-6 py-3 bg-green-600 rounded-3xl hover:bg-green-700 transition">
           Sign In
         </button>
       </form>
