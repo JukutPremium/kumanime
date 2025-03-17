5452f2ff6cd83cb8ad1138380b9db9f7e86bd4c7
Osi ic <osiic.offcl@gmail.com>
2025-03-17 09:18:44 +0700

    fix: error pasth

--- src/app/(routes)/(test)/button/page.jsx
+++ src/app/(routes)/(test)/button/page.jsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 
-export default function Button() {
+export default function ButtonPage() {
   return (
     <div className="flex justify-center items-center">
       <Button>Halo</Button>
