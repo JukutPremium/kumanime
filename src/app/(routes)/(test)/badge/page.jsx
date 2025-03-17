16cc979479b499819cb7dfedf2d9af3d031e5a6a
Osi ic <osiic.offcl@gmail.com>
2025-03-17 10:01:07 +0700

    style: show all variant badge

---
+++ src/app/(routes)/(test)/badge/page.jsx
@@ -0,0 +1,20 @@
+import { Badge } from "@/components/ui/badge"; // Sesuaikan dengan path yang benar
+import { AlarmClock } from "lucide-react";
+
+export default function BadgePage() {
+  return (
+    <div className="flex w-screen py-52 gap-4 flex-col flex-wrap justify-between container mx-auto items-center">
+      {/* Default Variant */}
+      <Badge variant="default">Default</Badge>
+
+      {/* Secondary Variant */}
+      <Badge variant="secondary">Secondary</Badge>
+
+      {/* Destructive Variant */}
+      <Badge variant="destructive">Destructive</Badge>
+
+      {/* Outline Variant */}
+      <Badge variant="outline">Outline</Badge>
+    </div>
+  );
+}
