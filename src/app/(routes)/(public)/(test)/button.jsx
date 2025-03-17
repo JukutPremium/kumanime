8b9e7bf6fdc93f30d74887087966a7ac7d939fd6
Osi ic <osiic.offcl@gmail.com>
2025-03-17 09:16:53 +0700

    feat: add shadch and test ting button

---
+++ src/app/(routes)/(public)/(test)/button.jsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Button() {
+  return (
+    <div className="flex justify-center items-center">
+      <Button>Halo</Button>
+    </div>
+  );
+}
