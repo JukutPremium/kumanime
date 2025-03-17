5714c43d511a870942455504bf2bcf4ff5b536ca
Osi ic <osiic.offcl@gmail.com>
2025-03-17 10:22:50 +0700

    feat: setup breadcrumb custom

---
+++ src/app/(routes)/(test)/breadcrumb/page.jsx
@@ -0,0 +1,30 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+export default function BreadcrumbTest() {
+  return (
+    <div className="flex w-screen py-52 justify-center container mx-auto items-center">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/">Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href="/components">Components</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+    </div>
+  );
+}
---
+++ src/app/(routes)/(test)/logo/page.jsx
@@ -0,0 +1,5 @@
+import { Logo } from "@/components/ui/logo";
+
+export default function LogoTest() {
+  return <Logo />;
+}
